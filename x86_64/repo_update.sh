f175f1c9373b936f0fded652bef1a3728ec9654a
Alexey Gumirov <ag_devops@die-optimisten.net>
2021-05-27 21:14:54 +0200

    db update script

---
+++ x86_64/repo_update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Updating neovim package
+cd neovim-nightly-bin/
+
+NVIM_NIGHTLY_VERSION="pkgver="$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/-/+/g; s/"//g')
+sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_VERSION}" PKGBUILD
+makepkg -s
+
+# Updating packages db
+cd ..
+
+rm alex-arch-repo.db
+rm alex-arch-repo.files
+
+repo-add alex-arch-repo.db.tar.gz -R $(/bin/ls -1 --sort=time neovim-nightly-bin/*.zst | head -n1)
+
+rm alex-arch-repo.db
+cp alex-arch-repo.db.tar.gz alex-arch-repo.db
+
+rm alex-arch-repo.files
+cp alex-arch-repo.files.tar.gz alex-arch-repo.files

9f1737532de41a8668836ca229982eb9a3b7bfee
Alexey Gumirov <ag_devops@die-optimisten.net>
2021-05-27 20:48:24 +0200

    gitignore

--- neovim-nightly-bin/pkgbuild_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-NVIM_NIGHTLY_VERSION="pkgver="$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/-/+/g; s/"//g')
-
-sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_VERSION}" PKGBUILD
-makepkg -si

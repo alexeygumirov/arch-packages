757a6af24c40df9d67a9fabbc46fa5b9ee8e3ff9
Alexey Gumirov <ag_devops@die-optimisten.net>
2021-06-03 13:40:19 +0200

    NeoVIM Nightly 0.5.0-dev+1372-g056c464e8

--- x86_64/repo_update.sh
+++ x86_64/repo_update.sh
@@ -8,8 +8,9 @@ rm neovim-*tar.zst
 ## Building new one
 cd neovim-nightly-bin/
 
-NVIM_NIGHTLY_VERSION="pkgver="$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/-/+/g; s/"//g')
-sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_VERSION}" PKGBUILD
+NVIM_NIGHTLY_VERSION=$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/"//g')
+NVIM_NIGHTLY_PKG_VERSION="pkgver="$(echo ${NVIM_NIGHTLY_VERSION} | sed 's/-/+/g')
+sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_PKG_VERSION}" PKGBUILD
 makepkg -s
 mv *.zst ../
 rm *.tar.gz

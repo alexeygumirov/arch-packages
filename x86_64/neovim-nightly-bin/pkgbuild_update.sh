1babfe2947bcc6434cf554a6309c75fe1aa71d9b
Alexey Gumirov <ag_devops@die-optimisten.net>
2022-01-29 09:20:37 +0100

    NeoVIM Nightly v0.7.0-dev+963-g082ff2190

--- x86_64/neovim-nightly-bin/pkgbuild_update.sh
+++ x86_64/neovim-nightly-bin/pkgbuild_update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NVIM_NIGHTLY_VERSION="pkgver="$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/-/+/g; s/"//g')
+NVIM_NIGHTLY_VERSION="pkgver="$(curl -s https://api.github.com/repos/neovim/neovim/releases | jq '.[0].body' | sed 's/\\n/\n/g' | grep NVIM | cut -d' ' -f2)
 
 sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_VERSION}" PKGBUILD
 makepkg -si
--- x86_64/repo_update.sh
+++ x86_64/repo_update.sh
@@ -8,7 +8,8 @@ rm neovim-*tar.zst
 ## Building new one
 cd neovim-nightly-bin/
 
-NVIM_NIGHTLY_VERSION=$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/"//g')
+# NVIM_NIGHTLY_VERSION=$(curl -s 'https://api.github.com/repos/neovim/neovim/releases' | jq '.[0].name' | sed 's/NVIM v//; s/"//g')
+NVIM_NIGHTLY_VERSION=$(curl -s https://api.github.com/repos/neovim/neovim/releases | jq '.[0].body' | sed 's/\\n/\n/g' | grep NVIM | cut -d' ' -f2)
 NVIM_NIGHTLY_PKG_VERSION="pkgver="$(echo ${NVIM_NIGHTLY_VERSION} | sed 's/-/+/g')
 sed -i "/^pkgver=*/c ${NVIM_NIGHTLY_PKG_VERSION}" PKGBUILD
 makepkg -s
